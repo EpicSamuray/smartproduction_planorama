9728f3f9dfbd1c28d32c53cf4fdacca8878f0dfc
Felice Pettinaro <felice@pettinaro.xyz>
2024-05-04 17:51:57 +0200

    feat(login): add Login screen to WindowsFrame

--- lib/view/windowsFrame.dart
+++ lib/view/windowsFrame.dart
@@ -2,7 +2,6 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:smartproduction_planorama/view/Login.dart';
-import 'package:smartproduction_planorama/view/dashboard.dart';
 import 'package:smartproduction_planorama/view/widget/windowsBar.dart';
 
 import '../viewModels/authProvider.dart';
@@ -17,7 +16,7 @@ class WindowsFrame extends ConsumerWidget {
         width: 1,
         child: Stack(
           children: [
-            const DashboardScreen(),
+            const LoginScreen(),
             const WindowsBar(),
           ],
         ),
